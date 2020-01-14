6be5d9765e4a96e0ae162fbe875e7d550475b0c3
Bijiabo <bijiabo@gmail.com>
2020-01-14 23:25:43 +0800

    resource: 增加图片资源

---
+++ src/page/hugeList/assets/material/index.ts
@@ -0,0 +1,29 @@
+import {ImageSourcePropType} from 'react-native'
+const resource:{[key: string]: ImageSourcePropType} = {
+  'Image_24.png': require('./Image_24.png'),
+  'Image_2.png': require('./Image_2.png'),
+  'Image_3.png': require('./Image_3.png'),
+  'Image_4.png': require('./Image_4.png'),
+  'Image_5.png': require('./Image_5.png'),
+  'Image_6.png': require('./Image_6.png'),
+  'Image_7.png': require('./Image_7.png'),
+  'Image_8.png': require('./Image_8.png'),
+  'Image_9.png': require('./Image_9.png'),
+  'Image_10.png': require('./Image_10.png'),
+  'Image_11.png': require('./Image_11.png'),
+  'Image_12.png': require('./Image_12.png'),
+  'Image_13.png': require('./Image_13.png'),
+  'Image_14.png': require('./Image_14.png'),
+  'Image_15.png': require('./Image_15.png'),
+  'Image_16.png': require('./Image_16.png'),
+  'Image_17.png': require('./Image_17.png'),
+  'Image_18.png': require('./Image_18.png'),
+  'Image_19.png': require('./Image_19.png'),
+  'Image_20.png': require('./Image_20.png'),
+  'Image_21.png': require('./Image_21.png'),
+  'Image_22.png': require('./Image_22.png'),
+  'Image_23.png': require('./Image_23.png'),
+  'Image_1.png': require('./Image_1.png'),
+};
+
+export default resource
